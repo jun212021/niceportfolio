453b5f8a25b5e1d762cb74861b0a6bf1fe6fc7d6
niceportfolio <niceportfolios@gmail.com>
2020-05-30 22:26:54 +0100

    Update folios.js

--- folios.js
+++ folios.js
@@ -1,20 +1,23 @@
   var links = [
-               "nathan.tokyo/",
-               "benlongdendesign.co.uk/",
-               "neal.fun/",
-               "michael.wang/",
-               "liebermannkiepe.de/en",
-               "sspeier.com/",
-               "adamho.com/",
-               "carolpychan.com/index.html",
-               "coryetzkorn.com/",
-               "sharypin.cool/",
-               "zefs.website/",
-               "kyledecker.me",
-               "brianbaker.com",
-               "taliacotton.com/work",
-               "twnsnd.co",
-               "karalevich.com"]
+               "https://nathan.tokyo/",
+               "https://neal.fun/",
+               "https://michael.wang/",
+               "https://liebermannkiepe.de/en",
+               "https://sspeier.com/",
+               "https://adamho.com/",
+               "https://carolpychan.com/index.html",
+               "https://coryetzkorn.com/",
+               "https://sharypin.cool/",
+               "https://zefs.website/",
+               "https://kyledecker.me",
+               "https://taliacotton.com/work",
+               "https://twnsnd.co",
+               "http://erichu.info/",
+               "http://elirousso.com/",
+               "https://timroussilhe.com/",
+               "https://stanleychen.info/",
+               "https://studiofrank.co",
+               "https://karalevich.com"]
 
             var openSite = function() {
                // get a random number between 0 and the number of links
@@ -22,7 +25,7 @@
                // round it, so it can be used as array index
                randIdx = parseInt(randIdx, 10);
                // construct the link to be opened
-               var link = 'http://' + links[randIdx];
+               var link = links[randIdx];
 
       var win = window.open(link, '_blank');
       win.focus();
